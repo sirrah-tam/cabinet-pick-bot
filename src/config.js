6da7a2eedf747f8144708cef510bdaf5f84583c7
Mat Harris <hurris88@gmail.com>
2017-02-05 20:38:06 +0000

    Initial Commit

---
+++ src/config.js
@@ -0,0 +1,21 @@
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+require('dotenv').config();
+
+// module.exports = {
+//   consumer_key: process.env.CONSUMER_KEY,
+//   consumer_secret: process.env.CONSUMER_SECRET,
+//   access_token: process.env.ACCESS_TOKEN,
+//   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+// };
+
+module.exports = {
+  consumer_key: 'zZegyHn2GHDbHs6slGdwSS7fi',
+  consumer_secret: 'NzgqIoqIMnLOKqiyXVjZI6nRqr5FiaBbIqIXKXHHBOZb0PUBsK',
+  access_token: '828315410885963777-g7GEjJ5xkhlJVq84s2tjV9kJwxtORDb',
+  access_token_secret: 'rFhWQPGvuSCwBlN1S3H9tT8QuvNmgm8aJTiEKwTXvHB6a',
+};
---
+++ src/helpers/strings.js
@@ -0,0 +1,41 @@
+module.exports = {
+  queryString: [
+    'cabinet',
+    'call your senator',
+    'floor vote',
+    'Sessions',
+    'DeVos',
+    'Mnuchin',
+    'Price',
+    'Puzder',
+    'Zinke',
+    'Perry',
+    'Carson',
+    'Pruitt',
+    'Perdue',
+    'Trump',
+  ],
+  queryStringSubQuery: [
+    ' pick',
+    ' oppose',
+  ],
+  blockedStrings: [
+    'porn',
+    'sex',
+    'confirm'
+  ],
+  resultType: [
+    'mixed',
+    'recent',
+    'popular'
+  ],
+  responseString: [
+    'Hi @screenName thanks for the follow!',
+    '@screenName thanks for following!',
+    'Hey @screenName thanks for the follow!',
+    'Thanks for following @screenName!',
+    'Hey @screenName, thanks for following!',
+    'Awesome @screenName, thanks for following!',
+    'Thanks for the follow @screenName!'
+  ]
+}
